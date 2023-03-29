d853475e2eaf10f4950de1fa8cb9c6ed9d709c9b
“nryota98s” <ryoutanumamura@gmail.com>
2023-03-29 13:33:33 +0900

    ma

--- resources/views/createForm.blade.php
+++ resources/views/createForm.blade.php
@@ -1,71 +1,50 @@
-<!DOCTYPE html>
+@extends('layouts/app')
 
-<html>
+@section('content')
+<div class='container'>
 
-
-<head>
-
-  <meta charset='utf-8"'>
-
-  <link rel='stylesheet' href='/css/app.css'>
-
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-
-</head>
-
-
-<body>
+  <h2 class='page-header'>新しく投稿をする</h2>
+  @if ($errors->any())
+  <div class="alert alert-danger">
+    <ul>
+      @foreach ($errors->all() as $error)
+      <li>{{ $error }}</li>
+      @endforeach
+    </ul>
+  </div>
+  @endif
 
 
-  <header>
+  {!! Form::open(['url' => 'post/create']) !!}
 
-    <h1 class='page-header'>サーバーサイド課題 沼村竜汰</h1>
+  <div class="form-group">
 
-  </header>
-
-  <div class='container'>
-
-    <h2 class='page-header'>新しく投稿をする</h2>
-    @if ($errors->any())
-    <div class="alert alert-danger">
-      <ul>
-        @foreach ($errors->all() as $error)
-        <li>{{ $error }}</li>
-        @endforeach
-      </ul>
-    </div>
-    @endif
+    {!! Form::input('text', 'userName', Auth:: user()->name, ['class' => 'form-control', 'required','placeholder' => 'ユーザーネーム']) !!}
 
 
-    {!! Form::open(['url' => 'post/create']) !!}
+    {!! Form::input('text', 'newPost', null, ['required', 'class' => 'form-control', 'placeholder' => '投稿内容','value'=>"{{ old('newPost') }}"]) !!}
 
-    <div class="form-group">
-
-      {!! Form::input('text', 'userName', Auth:: user()->name, ['class' => 'form-control', 'required','placeholder' => 'ユーザーネーム']) !!}
-
-
-      {!! Form::input('text', 'newPost', null, ['required', 'class' => 'form-control', 'placeholder' => '投稿内容','value'=>"{{ old('newPost') }}"]) !!}
-
-
-    </div>
-
-    <button type="submit" class="btn btn-success pull-right">追加</button>
-
-    {!! Form::close() !!}
 
   </div>
 
+  <button type="submit" class="btn btn-success pull-right">追加</button>
 
-  <footer>
+  {!! Form::close() !!}
+
+</div>
 
 
-  </footer>
+<footer>
 
-  <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 
-  <script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min.js"></script>
+</footer>
+
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+
+<script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min.js"></script>
 
 </body>
 
 
 </html>
+@endsection
